08a57fea5337ef864f1424787b6a949d643f3a07
Robert Yin <bobertoyin@gmail.com>
2023-10-11 22:38:11 -0500

    fix song language and lyrics_placeholder_reason

--- tests/integration.rs
+++ tests/integration.rs
@@ -179,10 +179,7 @@ async fn test_search(
 #[tokio::test]
 
 async fn test_song(
-    #[values(
-        7756301, 6691103, 7327436, 5444192, 7784464, 3892702, 2416243, 99999, 99999999
-    )]
-    id: u32,
+    #[values(2177076, 7756301, 6691103, 7327436, 5444192, 99999, 99999999)] id: u32,
     client: Client,
     reqwest_client: ReqwestClient,
 ) {
